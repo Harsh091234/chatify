bfb27b8dd31624dab895c495429e7d64b62a4a03
Harsh091234 <hsharma4090@gmai.com>
2025-08-13 19:26:00 +0530

    regex error fixed

--- server/src/server.js
+++ server/src/server.js
@@ -30,8 +30,9 @@ app.use('/api/user', userRoutes);
 app.use('/api/message', messageRoutes);
 
 if(process.env.NODE_ENV === "production"){
+ 
   app.use(express.static(path.join(__dirname, "../client/dist")));
-  app.get("*", (req, res) => {
+  app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, "../client", "dist", "index.html"))
   })
 }
